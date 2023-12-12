fe557a68b3d831404632efa92e029d28f2c0ab7e
gautam yadav <gy77044@gmail.com>
2023-12-12 22:48:29 +0530

    max subarray sum by prefix method

--- BinSearch/BinarySearch.java
+++ BinSearch/BinarySearch.java
@@ -96,25 +96,62 @@
 //   }
 // }
 
-//maximum subarray sum
+// // maximum subarray sum  --- by brute force
+
+// import java.util.*;
+
+// public class BinarySearch {
+//   public static void maxSubArraySum(int numbers[]) {
+//     int currSum = 0;
+//     int maxSum = Integer.MIN_VALUE;
+//     for (int i = 0; i < numbers.length; i++) {
+//       int start = i;
+//       for (int j = i; j < numbers.length; j++) {
+//         int end = j;
+//         currSum = 0;
+//         for (int k = start; k <= end; k++) {
+//           // System.out.print(numbers[k] + " ");
+//           // Subarray sum
+//           currSum = currSum + numbers[k];
+//         }
+//         System.out.println("curr sum = " + currSum);
+//         if (maxSum < currSum) {
+//           maxSum = currSum;
+//         }
+//       }
+//     }
+//     System.out.println("max sum = " + maxSum);
+//   }
+
+//   public static void main(String args[]) {
+//     int numbers[] = { 1, -2, 6, -1, 3 };
+//     maxSubArraySum(numbers);
+//   }
+// }
+
+// maximum subarray sum  --- by prefix method
 
 import java.util.*;
 
 public class BinarySearch {
   public static void maxSubArraySum(int numbers[]) {
     int currSum = 0;
-    int maxSum = Integer.MIN_VALUE;
+    int maxSum = Integer.MIN_VALUE;  
+    int prefix[] = new int[numbers.length];
+    
+    prefix[0] = numbers[0];
+    //calculate prefix array
+    for(int i=1;i<prefix.length;i++){
+      prefix[i]= prefix[i-1]+numbers[i];
+    }
     for (int i = 0; i < numbers.length; i++) {
       int start = i;
       for (int j = i; j < numbers.length; j++) {
         int end = j;
         currSum = 0;
-        for (int k = start; k <= end; k++) {
-          // System.out.print(numbers[k] + " ");
-          // Subarray sum
-          currSum = currSum + numbers[k];
-        }
-        System.out.println("curr sum = " + currSum);
+        
+        currSum= start==0? prefix[end]: prefix[end] - prefix[start-1];
+        
         if (maxSum < currSum) {
           maxSum = currSum;
         }
@@ -124,7 +161,7 @@ public class BinarySearch {
   }
 
   public static void main(String args[]) {
-    int numbers[] = { 1,-2,6,-1,3 };
+    int numbers[] = { 1, -2, 6, -1, 3 };
     maxSubArraySum(numbers);
   }
 }
