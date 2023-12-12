6196cd37f3183984d2c46aa1c2294e36b20acecb
gautam yadav <gy77044@gmail.com>
2023-12-13 00:15:19 +0530

    trappedRainWater

--- BinSearch/BinarySearch.java
+++ BinSearch/BinarySearch.java
@@ -166,27 +166,63 @@
 //   }
 // }
 
-//maximum subarray sum  --- by kadane's methods or kadan
+// //maximum subarray sum  --- by kadane's methods or kadan
+
+// import java.util.*;
+
+// public class BinarySearch {
+//   public static void kadane(int numbers[]) {
+//     int cs = 0;
+//     int ms = Integer.MIN_VALUE;
+
+//     for (int i = 0; i < numbers.length; i++) {
+//       cs = cs + numbers[i];
+//       if (cs < 0) {
+//         cs = 0;
+//       }
+//       ms = Math.max(cs, ms);
+//     }
+//     System.out.println("our max subarray sum is : " + ms);
+//   }
+
+//   public static void main(String args[]) {
+//     int numbers[] = {-2,-3,4,-1,-2,1,5,-3};
+//     kadane(numbers);
+//   }
+// }
+
+//trappedRainWater
 
 import java.util.*;
 
 public class BinarySearch {
-  public static void kadane(int numbers[]) {
-    int cs = 0;
-    int ms = Integer.MIN_VALUE;
-
-    for (int i = 0; i < numbers.length; i++) {
-      cs = cs + numbers[i];
-      if (cs < 0) {
-        cs = 0;
-      }
-      ms = Math.max(cs, ms);
+  public static int trappedRainWater(int height[]) {
+    int n = height.length;
+    // calculate left max boundary -array
+    int leftMax[] = new int[n];
+    leftMax[0] = height[0];
+    for (int i = 1; i < n; i++) {
+      leftMax[i] = Math.max(height[i], leftMax[i - 1]);
     }
-    System.out.println("our max subarray sum is : " + ms);
+    // calculate right max boundary -array
+    int rightMax[] = new int[n];
+    rightMax[n - 1] = height[n - 1];
+    for (int i = n - 2; i >= 0; i--) {
+      rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+    }
+    int trappedWater = 0;
+    // loop
+    for (int i = 0; i < n; i++) {
+      // waterLevel = min(leftmax bound, rightmax bound)
+      int waterLavel = Math.min(leftMax[i], rightMax[i]);
+      // trapped water = waterLevel-height[i]
+      trappedWater += waterLavel - height[i];
+    }
+    return trappedWater;
   }
 
   public static void main(String args[]) {
-    int numbers[] = {-2,-3,4,-1,-2,1,5,-3};
-    kadane(numbers);
+    int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+    System.out.println(trappedRainWater(height));
   }
 }
