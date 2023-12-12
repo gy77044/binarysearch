ba6ce59642422aa7a75e1629654766da8e8767af
gautam yadav <gy77044@gmail.com>
2023-12-12 23:27:33 +0530

    sum of sub arrays by kadane method

--- BinSearch/BinarySearch.java
+++ BinSearch/BinarySearch.java
@@ -131,37 +131,62 @@
 
 // maximum subarray sum  --- by prefix method
 
+// import java.util.*;
+
+// public class BinarySearch {
+//   public static void maxSubArraySum(int numbers[]) {
+//     int currSum = 0;
+//     int maxSum = Integer.MIN_VALUE;  
+//     int prefix[] = new int[numbers.length];
+
+//     prefix[0] = numbers[0];
+//     //calculate prefix array
+//     for(int i=1;i<prefix.length;i++){
+//       prefix[i]= prefix[i-1]+numbers[i];
+//     }
+//     for (int i = 0; i < numbers.length; i++) {
+//       int start = i;
+//       for (int j = i; j < numbers.length; j++) {
+//         int end = j;
+//         currSum = 0;
+
+//         currSum= start==0? prefix[end]: prefix[end] - prefix[start-1];
+
+//         if (maxSum < currSum) {
+//           maxSum = currSum;
+//         }
+//       }
+//     }
+//     System.out.println("max sum = " + maxSum);
+//   }
+
+//   public static void main(String args[]) {
+//     int numbers[] = { 1, -2, 6, -1, 3 };
+//     maxSubArraySum(numbers);
+//   }
+// }
+
+//maximum subarray sum  --- by kadane's methods or kadan
+
 import java.util.*;
 
 public class BinarySearch {
-  public static void maxSubArraySum(int numbers[]) {
-    int currSum = 0;
-    int maxSum = Integer.MIN_VALUE;  
-    int prefix[] = new int[numbers.length];
-    
-    prefix[0] = numbers[0];
-    //calculate prefix array
-    for(int i=1;i<prefix.length;i++){
-      prefix[i]= prefix[i-1]+numbers[i];
-    }
+  public static void kadane(int numbers[]) {
+    int cs = 0;
+    int ms = Integer.MIN_VALUE;
+
     for (int i = 0; i < numbers.length; i++) {
-      int start = i;
-      for (int j = i; j < numbers.length; j++) {
-        int end = j;
-        currSum = 0;
-        
-        currSum= start==0? prefix[end]: prefix[end] - prefix[start-1];
-        
-        if (maxSum < currSum) {
-          maxSum = currSum;
-        }
+      cs = cs + numbers[i];
+      if (cs < 0) {
+        cs = 0;
       }
+      ms = Math.max(cs, ms);
     }
-    System.out.println("max sum = " + maxSum);
+    System.out.println("our max subarray sum is : " + ms);
   }
 
   public static void main(String args[]) {
-    int numbers[] = { 1, -2, 6, -1, 3 };
-    maxSubArraySum(numbers);
+    int numbers[] = {-2,-3,4,-1,-2,1,5,-3};
+    kadane(numbers);
   }
 }
